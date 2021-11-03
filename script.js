db9322dba1aecc35a85731b0813cfd0aa324880d
arbartl <knoxbartl@gmail.com>
2021-11-02 19:54:50 -0500

    Added basic HTML and javascript functions

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(num1, num2) {
+    total = num1 + num2;
+    return total;
+}
+
+function subtract(num1, num2) {
+    total = num1 - num2;
+    return total;
+}
+
+function multiply(num1, num2) {
+    total = num1 * num2;
+    return total;
+}
+
+function divide(num1, num2) {
+    total = num1 / num2;
+    return total;
+}
+
+function operate(num1, operator, num2) {
+    if (operator == '+') {
+        return add(num1, num2);
+    } else if (operator == '-') {
+        return subtract(num1, num2);
+    } else if (operator == '*') {
+        return multiply(num1, num2);
+    } else if (operator == '/') {
+        return divide(num1, num2);
+    }
+}
